22bcab9bd4060191ee9c17163392f6d4cafc287a
arkans <82564511+arkansys22@users.noreply.github.com>
2022-08-31 12:02:24 +0700

    up

--- application/controllers/Petacrawl.php
+++ application/controllers/Petacrawl.php
@@ -6,9 +6,8 @@ class Petacrawl extends CI_Controller {
  public function index(){
      $this->load->helper('url');
      $this->load->model('Sitemap_model');
-     $data['blogs'] = $this->Sitemap_model->generate_blogs('blogs');
-     $data['services'] = $this->Sitemap_model->generate_services('services');
-     $data['works'] = $this->Sitemap_model->generate_works('products');
+     $data['products'] = $this->Sitemap_model->generate('products','products_judul_seo','products_id');
+     $data['products_cat'] = $this->Sitemap_model->generate('products_cat','products_cat_judul_seo','products_cat_id');
      $this->load->view('v_sitemap',$data);
  }
 
--- application/views/v_sitemap.php
+++ application/views/v_sitemap.php
@@ -6,43 +6,35 @@
      <priority>1.0</priority>
   </url>
   <url>
-     <loc><?php echo base_url("works/");?></loc>
-     <priority>0.5</priority>
-  </url>
-  <?php foreach($works as $data) { ?>
-  <url>
-     <loc><?php echo base_url("works/").$data->products_judul_seo;?></loc>
-     <priority>0.5</priority>
-  </url>
-  <?php } ?>
-  <url>
-     <loc><?php echo base_url("services/");?></loc>
-     <priority>0.5</priority>
-  </url>
-  <?php foreach($services as $data) { ?>
-  <url>
-     <loc><?php echo base_url("services/").$data->services_judul_seo;?></loc>
-     <priority>0.5</priority>
-  </url>
-  <?php } ?>
-  <url>
-     <loc><?php echo base_url("reviews/");?></loc>
-     <priority>0.5</priority>
-  </url>
-  <url>
      <loc><?php echo base_url("about/");?></loc>
      <priority>0.5</priority>
   </url>
   <url>
-     <loc><?php echo base_url("blogs/");?></loc>
+     <loc><?php echo base_url("gallery/");?></loc>
      <priority>0.5</priority>
   </url>
-  <?php foreach($blogs as $data) { ?>
   <url>
-     <loc><?php echo base_url("blogs/").$data->blogs_judul_seo;?></loc>
+     <loc><?php echo base_url("equipment/");?></loc>
+     <priority>0.5</priority>
+  </url>
+  <url>
+     <loc><?php echo base_url("workshop/");?></loc>
+     <priority>0.5</priority>
+  </url>
+  <?php foreach($products as $data) { ?>
+  <url>
+     <loc><?php echo base_url("portfolio-detail/").$data->products_judul_seo;?></loc>
      <priority>0.5</priority>
   </url>
   <?php } ?>
+  <?php foreach($products_cat as $data) { ?>
+  <url>
+     <loc><?php echo base_url("portfolio/").$data->products_cat_judul_seo;?></loc>
+     <priority>0.5</priority>
+  </url>
+  <?php } ?>
+
+
 
 
 </urlset>
