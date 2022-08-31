41cbdc56b2e9098c42be25cf56f3d34c33c0fc7b
arkans <82564511+arkansys22@users.noreply.github.com>
2022-08-31 12:03:24 +0700

    up

--- application/controllers/Petacrawl.php
+++ application/controllers/Petacrawl.php
@@ -7,7 +7,7 @@ class Petacrawl extends CI_Controller {
      $this->load->helper('url');
      $this->load->model('Sitemap_model');
      $data['products'] = $this->Sitemap_model->generate('products','products_judul_seo','products_id');
-     $data['products_cat'] = $this->Sitemap_model->generate('products_cat','products_cat_judul_seo','products_cat_id');
+     $data['products_cat'] = $this->Sitemap_model->generate('products_category','products_cat_judul_seo','products_cat_id');
      $this->load->view('v_sitemap',$data);
  }
 
